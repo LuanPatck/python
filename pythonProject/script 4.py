8def80b29a1b33a2c6c691e9ff38f6b14e43a5f6
Luan Patrick <139468410+LuanPatck@users.noreply.github.com>
2023-12-27 12:47:34 -0300

    exercicio 4

---
+++ pythonProject/script 4.py
@@ -0,0 +1,9 @@
+numero_1 = int(input("Digite um número:"))
+numero_2 = int(input("Digite outro número:"))
+
+soma = numero_1 + numero_2
+sub = numero_1 - numero_2
+mult = numero_1 * numero_2
+div = numero_1 / numero_2
+
+print(" A Soma dos números são {}, a Subtração é {}, a Multiplicação é {}, a Divisão é {}". format(soma,sub,mult,div))
